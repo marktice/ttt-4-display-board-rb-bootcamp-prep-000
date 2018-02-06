3c342fe562fab67e8eb8ad48b8f18ca99bbc5322
Mark Tice <marktice@protonmail.com>
2018-02-06 02:47:46 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,9 +6,7 @@ def display_board (input)
   puts " #{input[3]} " + "|" + " #{input[4]} " + "|" + " #{input[5]} "
   puts "-"*11
   puts " #{input[6]} " + "|" + " #{input[7]} " + "|" + " #{input[8]} "
-  
 end
-
 
 board = [" "," "," "," "," "," "," "," "," "]
 display_board(board)
